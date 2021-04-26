ada57d32ede68729187305441ee32c4bcddef0db
Saurabh kumar <saurabhofficial@hotmail.com>
2021-04-26 23:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,15 @@ app.use(bodyParser.urlencoded({
 // using a public folder to deliver static assets 
 app.use(express.static("public"));
 
-
 // connecting to the db
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});
 
 // now code specific to a server
 
-const articleSchema={ 
+const articleSchema=new mongoose.Schema({ 
     name:String,
     content:String
-};
+});
 
 // this adds an s to the back of "Article" and lowercases all the letters on its own and creates a collection 
 // named articles if not already made 
